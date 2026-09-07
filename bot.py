68c2d8488f9c83f53e127367fd8412f9f4bae7d8
juliendjidonou7-blip <juliendjidonou7@gmail.com>
2026-09-07 17:08:36 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,8 @@
+import time
+
+print("=== SMC_Bot_V3 lance avec succes sur Render ===")
+
+while True:
+    print(f"[{time.strftime('%H:%M:%S')}] Analyse du Bias H4 et du BOS H1 sur XAUUSD...")
+    # Pause de 5 minutes pour économiser votre temps de serveur gratuit
+    time.sleep(300)
